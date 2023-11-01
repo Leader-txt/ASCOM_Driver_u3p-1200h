788ed630a9ddeb680aeabf92efba0cebb26c4ca5
Leader <3251263056@qq.com>
2023-11-01 20:15:29 +0800

    first upload project

---
+++ Driver_u3p1200h/CameraDriver/SetupDialogForm.cs
@@ -0,0 +1,154 @@
+using ASCOM.Utilities;
+using DVPCameraType;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ASCOM.LeaderU3P1200H.Camera
+{
+    [ComVisible(false)] // Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        const string NO_PORTS_MESSAGE = "No COM ports found";
+        TraceLogger tl; // Holder for a reference to the driver's trace logger
+
+        public SetupDialogForm(TraceLogger tlDriver)
+        {
+            InitializeComponent();
+
+            // Save the provided trace logger for use within the setup dialogue
+            tl = tlDriver;
+
+            // Initialise current values of user settings from the ASCOM Profile
+            InitUI();
+        }
+
+        private void CmdOK_Click(object sender, EventArgs e) // OK button event handler
+        {
+            // Place any validation constraint checks here and update the state variables with results from the dialogue
+
+            tl.Enabled = chkTrace.Checked;
+
+            // Update the COM port variable if one has been selected
+            if (comboBoxComPort.SelectedItem is null) // No COM port selected
+            {
+                tl.LogMessage("Setup OK", $"New configuration values - COM Port: Not selected");
+            }
+            else if (comboBoxComPort.SelectedItem.ToString() == NO_PORTS_MESSAGE)
+            {
+                tl.LogMessage("Setup OK", $"New configuration values - NO COM ports detected on this PC.");
+            }
+            else // A valid COM port has been selected
+            {
+                CameraHardware.comPort = (string)comboBoxComPort.SelectedItem;
+                tl.LogMessage("Setup OK", $"New configuration values - COM Port: {comboBoxComPort.SelectedItem}");
+            }
+        }
+
+        private void CmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+        {
+            Close();
+        }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://ascom-standards.org/");
+            }
+            catch (Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+
+        private void InitUI()
+        {
+            Scan();
+            // Set the trace checkbox
+            /*chkTrace.Checked = tl.Enabled;
+
+            // set the list of COM ports to those that are currently available
+            comboBoxComPort.Items.Clear(); // Clear any existing entries
+            using (Serial serial = new Serial()) // User the Se5rial component to get an extended list of COM ports
+            {
+                comboBoxComPort.Items.AddRange(serial.AvailableCOMPorts);
+            }
+
+            // If no ports are found include a message to this effect
+            if (comboBoxComPort.Items.Count == 0)
+            {
+                comboBoxComPort.Items.Add(NO_PORTS_MESSAGE);
+                comboBoxComPort.SelectedItem = NO_PORTS_MESSAGE;
+            }
+
+            // select the current port if possible
+            if (comboBoxComPort.Items.Contains(CameraHardware.comPort))
+            {
+                comboBoxComPort.SelectedItem = CameraHardware.comPort;
+            }
+
+            tl.LogMessage("InitUI", $"Set UI controls to Trace: {chkTrace.Checked}, COM Port: {comboBoxComPort.SelectedItem}");*/
+        }
+
+        private void SetupDialogForm_Load(object sender, EventArgs e)
+        {
+            // Bring the setup dialogue to the front of the screen
+            if (WindowState == FormWindowState.Minimized)
+                WindowState = FormWindowState.Normal;
+            else
+            {
+                TopMost = true;
+                Focus();
+                BringToFront();
+                TopMost = false;
+            }
+        }
+        private void Scan()
+        {
+            uint n = 0;
+            dvpStatus status = DVPCamera.dvpRefresh(ref n);
+            dvpCameraInfo info = new dvpCameraInfo();
+            comboBoxComPort.Items.Clear();
+            if (status == dvpStatus.DVP_STATUS_OK)
+            {
+                for (uint i = 0; i < n; i++)
+                {
+                    status = DVPCamera.dvpEnum(i, ref info);
+                    comboBoxComPort.Items.Add(info.FriendlyName);
+                    if (i == 0)
+                        comboBoxComPort.Text = info.FriendlyName;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Scan();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CameraHardware.IsValidHandle(CameraHardware.handle))
+            {
+                DVPCamera.dvpShowPropertyModalDialog(CameraHardware.handle, this.Handle);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var status = DVPCamera.dvpOpenByName(comboBoxComPort.Text, dvpOpenMode.OPEN_NORMAL, ref CameraHardware.handle);
+            if (status == dvpStatus.DVP_STATUS_OK)
+                Close();
+        }
+    }
+}
---
+++ Driver_u3p1200h/CameraDriver/SetupDialogForm.designer.cs
@@ -0,0 +1,173 @@
+namespace ASCOM.LeaderU3P1200H.Camera
+{
+    partial class SetupDialogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetupDialogForm));
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.picASCOM = new System.Windows.Forms.PictureBox();
+            this.comboBoxComPort = new System.Windows.Forms.ComboBox();
+            this.chkTrace = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdOK.Location = new System.Drawing.Point(422, 155);
+            this.cmdOK.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(88, 33);
+            this.cmdOK.TabIndex = 0;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.CmdOK_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.Location = new System.Drawing.Point(422, 197);
+            this.cmdCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(88, 35);
+            this.cmdCancel.TabIndex = 1;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.CmdCancel_Click);
+            // 
+            // picASCOM
+            // 
+            this.picASCOM.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picASCOM.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picASCOM.Image = ((System.Drawing.Image)(resources.GetObject("picASCOM.Image")));
+            this.picASCOM.Location = new System.Drawing.Point(438, 12);
+            this.picASCOM.Margin = new System.Windows.Forms.Padding(4);
+            this.picASCOM.Name = "picASCOM";
+            this.picASCOM.Size = new System.Drawing.Size(48, 56);
+            this.picASCOM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picASCOM.TabIndex = 3;
+            this.picASCOM.TabStop = false;
+            this.picASCOM.Click += new System.EventHandler(this.BrowseToAscom);
+            this.picASCOM.DoubleClick += new System.EventHandler(this.BrowseToAscom);
+            // 
+            // comboBoxComPort
+            // 
+            this.comboBoxComPort.FormattingEnabled = true;
+            this.comboBoxComPort.Location = new System.Drawing.Point(13, 12);
+            this.comboBoxComPort.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxComPort.Name = "comboBoxComPort";
+            this.comboBoxComPort.Size = new System.Drawing.Size(198, 26);
+            this.comboBoxComPort.TabIndex = 7;
+            // 
+            // chkTrace
+            // 
+            this.chkTrace.AutoSize = true;
+            this.chkTrace.Location = new System.Drawing.Point(13, 210);
+            this.chkTrace.Margin = new System.Windows.Forms.Padding(4);
+            this.chkTrace.Name = "chkTrace";
+            this.chkTrace.Size = new System.Drawing.Size(106, 22);
+            this.chkTrace.TabIndex = 6;
+            this.chkTrace.Text = "Trace on";
+            this.chkTrace.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "…®√Ë";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "¥Úø™";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "≈‰÷√";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SetupDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 242);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxComPort);
+            this.Controls.Add(this.chkTrace);
+            this.Controls.Add(this.picASCOM);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SetupDialogForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LeaderU3P1200H Setup";
+            this.Load += new System.EventHandler(this.SetupDialogForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.PictureBox picASCOM;
+        private System.Windows.Forms.ComboBox comboBoxComPort;
+        private System.Windows.Forms.CheckBox chkTrace;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
